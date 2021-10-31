49318d171a5b88443d7b6ec5d063f005729c6545
sekozak <seba84116@gmail.com>
2021-10-31 21:09:51 +0100

    lab02

--- src/main/java/agh/ics/oop/OptionsParser.java
+++ src/main/java/agh/ics/oop/OptionsParser.java
@@ -1,9 +1,10 @@
 package agh.ics.oop;
 
 public class OptionsParser {
-    private int count=0, i=0;
 
     public MoveDirection[] parse(String[] args){
+        int count = 0;
+        int i = 0;
         for(String q : args ) {
             if ( q.equals("f") || q.equals("forward") || q.equals("b") || q.equals("backward") ||
                     q.equals("r") || q.equals("right") || q.equals("l") || q.equals("left") ) count++;
--- src/test/java/agh/ics/oop/OptionsParserTest.java
+++ src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,17 +8,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class OptionsParserTest {
     private int i=0;
 
+    private OptionsParser parser;
+    private String[] args1;
+    private String[] args2;
+    private String[] args3;
+    private MoveDirection[] res1;
+    private MoveDirection[] res2;
+    private MoveDirection[] res3;
+
     @Test
-    void parsetest() {
-        String[] tab1 = new String[]{"f","r","left","7","b"};
-        MoveDirection[] tab2 = new MoveDirection[]{MoveDirection.FORWARD,MoveDirection.RIGHT,MoveDirection.LEFT,MoveDirection.BACKWARD};
-
-        OptionsParser parser = new OptionsParser();
-        MoveDirection[] tab3 = parser.parse(tab1);
-
-        for(MoveDirection q : tab3 ){
-            assertEquals(q , tab2[i]);
+    void parse() {
+        MoveDirection[] tab1 = parser.parse(args1);
+        for(MoveDirection q : tab1 ){
+            assertEquals(q , res1[i]);
             i++;
         }
+
+        i=0;
+        MoveDirection[] tab2 = parser.parse(args2);
+        for(MoveDirection q : tab2 ){
+            assertEquals(q , res2[i]);
+            i++;
+        }
+
+        i=0;
+        MoveDirection[] tab3 = parser.parse(args3);
+        for(MoveDirection q : tab3 ){
+            assertEquals(q , res3[i]);
+            i++;
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        parser = new OptionsParser();
+        args1 = new String[]{"f", "r", "r", "l", "l", "b", "c"};
+        args2 = new String[]{"f", "f", "b", "q", "w", "b", "a"};
+        args3 = new String[]{"forward", "b", "right", "l"};
+
+        res1 = new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.RIGHT, MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.LEFT, MoveDirection.BACKWARD};
+        res2 = new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.BACKWARD};
+        res3 = new MoveDirection[]{MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.LEFT};
     }
 }
